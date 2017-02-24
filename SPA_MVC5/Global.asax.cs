9e345595c63820a6a17123426098bd4dc993f436
tobi new <tmaxnoda@gmail.com>
2017-02-24 16:08:03 +0100

    some little changes with mvc packages update

--- SPA_MVC5/Global.asax.cs
+++ SPA_MVC5/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace SPA_MVC5
 {
@@ -12,6 +13,7 @@ namespace SPA_MVC5
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
